d554f4f5079f8cfe2e798e89175839a08c7a731f
Roy <rott.9954@gmail.com>
2022-02-21 18:15:58 -0600

    Fixes to Unused Imports

--- src/ES-EN/i18n.js
+++ src/ES-EN/i18n.js
@@ -1,11 +1,8 @@
-import React from "react"
 import i18n from "i18next";
 import { initReactI18next } from "react-i18next";
-import Cookies from "universal-cookie"
 import LanguageDetector from "i18next-browser-languagedetector";
 import * as enL from "./en.json";
 import * as esL from "./es.json";
-const cookie = new Cookies();
 
 
 i18n.use(initReactI18next).use(LanguageDetector).init({
--- src/components/ModalCards/ModalCards.jsx
+++ src/components/ModalCards/ModalCards.jsx
@@ -1,6 +1,5 @@
 import React, {useState} from "react";
 import Box from "@mui/material/Box";
-import Typography from "@mui/material/Typography";
 import Modal from "@mui/material/Modal";
 import { supabase } from "../../config/supabaseClient";
 import Swal from "sweetalert2";
